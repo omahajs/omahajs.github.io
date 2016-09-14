83c348005989f5075065d00ecd9413b7875d48ac
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-09-13 21:15:09 -0500

    Refined nightwatch npm workflow

--- test/nightwatch/tests/quick-test.js
+++ test/nightwatch/tests/quick-test.js
@@ -3,8 +3,8 @@ module.exports = {
       browser
         .url(browser.launch_url)
         .waitForElementVisible('.omaha-home', 1000)
-        .getTitle(function(title) {
-            this.assert.equal(title, 'Good Life. Great Code.');
+        .getTitle(function() {
+            this.assert.title('Good Life. Great Code.');
         })
         .end();
     }
