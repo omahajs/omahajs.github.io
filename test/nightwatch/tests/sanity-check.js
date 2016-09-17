3d7ca77cec9b20903a68e07371a7d3f2c2abd30b
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-09-16 21:14:42 -0500

    Added scroll to top tests

--- test/nightwatch/tests/sanity-check.js
+++ test/nightwatch/tests/sanity-check.js
@@ -4,8 +4,6 @@ var assert = chai.assert;
 module.exports = {
     tags: ['sanity'],
     'Sanity Check': function(browser) {
-        'use strict';
-
         browser
             .url(browser.launch_url)
             .resizeWindow(1200, 1000)
@@ -14,7 +12,14 @@ module.exports = {
             .getLocation('footer', function(result) {
                 assert(result.value.y > 1000, 'Footer should not be visible');
             });
-
+    },
+    'Scroll to Top': function(browser) {
+        browser
+            .assert.hidden('.scroll-to-top')
+            .execute('scrollTo(0, 3000)')
+            .assert.visible('.scroll-to-top')
+            .execute('scrollTo(0, 0)')
+            .assert.hidden('.scroll-to-top');
     },
     'End': function(browser) {
         browser.end();
