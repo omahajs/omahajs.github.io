348754bd25fd850e3e8550e3ce05da006ea6a96e
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-09-24 17:27:30 -0500

    Refining post style and adding share buttons

--- app/content.js
+++ app/content.js
@@ -2,7 +2,7 @@ define({
     news: [
         {
             type: 'post',
-            title: 'Test Announcement A',
+            title: 'Test Announcement #1',
             subtitle: 'This is a test of the news section',
             body: [
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -12,7 +12,7 @@ define({
         },
         {
             type: 'post',
-            title: 'Test Announcement B',
+            title: 'Test Announcement #2',
             subtitle: 'This is a test of the news section',
             body: [
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
