86fa52d91bab54a8bb9ef588a38893434ae0524d
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-09-24 19:21:31 -0500

    Added share buttons to post items

--- app/content.js
+++ app/content.js
@@ -2,8 +2,14 @@ define({
     news: [
         {
             type: 'post',
+            data: 1234,
             title: 'Test Announcement #1',
             subtitle: 'This is a test of the news section',
+            share: {
+                linkedin: 'Professional article for LinkedIn',
+                facebook: 'Interesting info to post',
+                twitter: 'Neat thing to tweet'
+            },
             body: [
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
--- app/views/Item.js
+++ app/views/Item.js
@@ -15,6 +15,16 @@ define(function(require, exports, module) {
         className: 'item-container',
         template: JST['content/item'],
         model: (new Item.Model()),
+        events: {
+            'click button.social': 'share'
+        },
+        templateContext: function() {
+            return {
+                uriEncode: function(str) {
+                    return encodeURIComponent(str);
+                }
+            };
+        },
         onRender: function() {
             var view = this;
             var parent = view._parent;
@@ -25,6 +35,31 @@ define(function(require, exports, module) {
                     .value();
                 view.$el.attr('data-type', type);
             }
+        },
+        share: function(e) {
+            var view = this;
+            var $e = $(e.currentTarget);
+            var name = $e.attr('data-social');
+            var sn_root = {
+                facebook: 'http://www.facebook.com/sharer/sharer.php',
+                linkedin: 'https://www.linkedin.com/shareArticle',
+                twitter: 'https://twitter.com/intent/tweet'
+            };
+            var sn = {
+                root: sn_root[name],
+                name: name,
+                meta: view.model.get('share')[name],
+                title: encodeURIComponent(view.model.get('title')),
+                summary: encodeURIComponent(view.model.get('subtitle')),
+                link: 'https://omaha.js.org'
+            };
+            // twitter text must be less than 140 characters
+            var uri = {
+                linkedin: sn.root + '?mini=true&url=' + sn.link + '&title=' + sn.title + '&summary=' + sn.summary + '&source=omahajs',
+                facebook: sn.root + '?u=' + sn.link + '&title=' + sn.title + '',
+                twitter:  sn.root + '?text=' + sn.title + '&via=omaha_js' + '&hashtags=GoodLifeGreatCode'
+            };
+            window.open(uri[name]);
         }
     });
 
