db8b37776950dd1b4d3a95f1c758bb154ed25d12
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-09-28 17:36:03 -0500

    Added url to twitter and linkedin buttons and rounded edges for project items

--- app/plugins/mn.share.behavior.js
+++ app/plugins/mn.share.behavior.js
@@ -22,16 +22,15 @@ define(function(require, exports, module) {
             var sn = {
                 root: sn_root[name],
                 name: name,
-                meta: view.model.get('share')[name],
                 title: encodeURIComponent(view.model.get('title')),
                 summary: encodeURIComponent(view.model.get('subtitle')),
-                link: 'https://omaha.js.org'
+                url: encodeURIComponent(window.location.href + '/' + view.model.get('postId'))
             };
             // twitter text must be less than 140 characters
             var uri = {
-                linkedin: sn.root + '?mini=true&url=' + sn.link + '&title=' + sn.title + '&summary=' + sn.summary + '&source=omahajs',
-                facebook: sn.root + '?u=' + sn.link + '&title=' + sn.title + '',
-                twitter:  sn.root + '?text=' + sn.title + '&via=omaha_js' + '&hashtags=GoodLifeGreatCode'
+                linkedin: sn.root + '?mini=true&url=' + sn.url + '&title=' + sn.title + '&summary=' + sn.summary + '&source=omahajs',
+                facebook: sn.root + '?u=' + sn.url + '&title=' + sn.title + '',
+                twitter:  sn.root + '?text=' + sn.title + '&via=omaha_js' + '&hashtags=GoodLifeGreatCode' + '&url=' + sn.url
             };
             window.open(uri[name]);
         }
