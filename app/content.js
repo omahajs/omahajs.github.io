52a153a824c0bc6896e4ad707bb37390237420dc
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-09-28 18:00:20 -0500

    Added space to end of last feed item

--- app/content.js
+++ app/content.js
@@ -6,11 +6,6 @@ define({
             date: '20160925',
             title: 'Really Long Test Announcement #1',
             subtitle: 'This is a test of the news section',
-            share: {
-                linkedin: 'Professional article for LinkedIn',
-                facebook: 'Interesting info to post',
-                twitter: 'Neat thing to tweet'
-            },
             body: [
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
             ]
