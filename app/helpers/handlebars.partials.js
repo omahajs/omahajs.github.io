32a29ec66ea75fef95d23e25179768d4510e3b13
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-10-06 17:34:53 -0500

    Initial addition of handlebars partial code

---
+++ app/helpers/handlebars.partials.js
@@ -0,0 +1,27 @@
+/**
+ * @file Handlebars partial templates
+ * @author Jason Wohlgemuth
+**/
+define(function(require) {
+    'use strict';
+
+    var $          = require('jquery');
+    var Handlebars = require('handlebars');
+
+    Handlebars.registerPartial('image', function(ctx) {
+        return '<img src="" alt="test"></img>';
+    });
+    Handlebars.registerPartial('quote', function(txt) {
+        return '<i>'+ txt +'</i>';
+    });
+    Handlebars.registerPartial('list', function(items) {
+        var $ul = $('<ul></ul>');
+        var $li = $('<li></li>');
+        Array.isArray(items) && items.forEach(function(item) {
+            $li.clone()
+                .text(item)
+                .appendTo($ul);
+        });
+        return $ul.html();
+    });
+});
