d88f27006e591de1208c2ae36c2d9bae9851be37
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-10-19 19:08:49 -0500

    Initial gist loading

--- app/helpers/handlebars.partials.js
+++ app/helpers/handlebars.partials.js
@@ -24,17 +24,6 @@ define(function(require) {
         return Array.prototype.slice.apply(arguments);
     }
 
-    var ID = '80b30714c33b197df0b6';
-    var GITHUB_API_URL = 'https://api.github.com/gists/';
-    function getGist(id) {
-        return $.ajax({
-            url: GITHUB_API_URL + id
-        }).then(function(data) {
-            var files = data.files;
-            return files[Object.keys(files)[0]].content;
-        });
-    }
-
     Handlebars.registerPartial('paragraph', function(txt) {
         var $p = $('<p></p>').text(txt);
         return $p[0].outerHTML;
@@ -43,12 +32,10 @@ define(function(require) {
         var $pre = $PRE.clone();
         var $wrap = $wrapper.clone()
             .addClass('code-block')
+            .attr('data-gist-id', id)
             .append($pre);
-        getGist(id).done(function(code) {
-            console.log(code);
-        });
         $CODE.clone()
-            .text('hello')
+            .text('https://gist.github.com/jhwohlgemuth/' + id + '.js')
             .appendTo($pre);
         return $wrap[0].outerHTML;
     });
--- app/views/Item.js
+++ app/views/Item.js
@@ -7,6 +7,7 @@
 define(function(require, exports, module) {
     'use strict';
 
+    var $      = require('jquery');
     var _      = require('underscore');
     var hljs   = require('highlightjs');
     var Mn     = require('backbone.marionette');
@@ -15,6 +16,16 @@ define(function(require, exports, module) {
     var moment = require('moment');
     var JST    = require('templates');
     var Item   = require('models/Item');
+
+    var GITHUB_API_URL = 'https://api.github.com/gists/';
+    function getGist(id) {
+        return $.ajax({
+            url: GITHUB_API_URL + id
+        }).then(function(data) {
+            var files = data.files;
+            return files[Object.keys(files)[0]].content;
+        });
+    }
 
     var ItemView = Mn.View.extend({
         className: 'item-container',
@@ -34,7 +45,7 @@ define(function(require, exports, module) {
         initialize: function() {
             hljs.configure({
                 tabReplace: '    ',
-                languages: ['html', 'css', 'less', 'javascript']
+                languages: ['javascript', 'html', 'css']
             });
         },
         onRender: function() {
@@ -50,6 +61,7 @@ define(function(require, exports, module) {
                     .attr('data-post-id', view.model.get('postId'))
                     .attr('data-post-name', view.model.get('name'));
             }
+            view.renderGists();
         },
         onDomRefresh: function() {
             $('pre code').each(function(i, block) {
@@ -63,6 +75,17 @@ define(function(require, exports, module) {
                 var name = view.$el.attr('data-post-name');
                 omaha.router.navigate('feed/' + name, {trigger: true});
             }
+        },
+        renderGists: function() {
+            var view = this;
+            view.$('[data-gist-id]').each(function(i, gist) {
+                var $gist = $(gist);
+                var id = $gist.attr('data-gist-id');
+                getGist(id).done(function(code) {
+                    $gist.find('code').text(code);
+                    view.triggerMethod('dom:refresh');
+                });
+            });
         }
     });
 
