1e966f4cd102e4d1b697fd7830d5b94c1c610217
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-10-21 20:04:38 -0500

    Fixed clipboard functionality

--- app/plugins/clipboard.behavior.js
+++ app/plugins/clipboard.behavior.js
@@ -12,17 +12,16 @@ define(function(require, exports, module) {
         },
         onCopy: function(e) {
             var view = this.view;
-            var $code = $(e.currentTarget).parent().siblings('pre');
+            var $content = $(e.currentTarget).parent().siblings('pre');
             var range = document.createRange();
             var selection = window.getSelection();
             view.$el.one('copy', function(e) {
-                e.preventDefault();
                 var selection = window.getSelection().toString();
-                (selection === $code.text()) && alert('Code copied to clipboard');
+                (selection === $content.text()) && alert('Copied to clipboard');
                 e.originalEvent.clipboardData.setData('text/plain', selection);
             });
             selection.removeAllRanges();// Clear selection from any previous data
-            range.selectNodeContents($code[0]);
+            range.selectNodeContents($content[0]);
             selection.addRange(range);// Add that range to the selection
             document.execCommand('copy');// Copy the selection to clipboard
             selection.removeAllRanges();
