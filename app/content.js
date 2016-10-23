50eb81f46873664efd0292312dae61d921757cbe
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-10-23 14:54:58 -0500

    More about section tweaking

--- app/content.js
+++ app/content.js
@@ -125,7 +125,7 @@ define(function(require, exports) {
                 paragraph('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
                 quote('Web technologies are awesome.'),
                 code(
-                    '//Expressed using JavaScript',
+                    '//Expressed with functional, point-free, and fluent JS',
                     'let tech = [',
                     '\t\'JavaScript\',',
                     '\t\'Node\',',
