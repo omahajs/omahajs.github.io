85dce1ca2e660a6035f2bd444b081c7c9bf39fa1
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-10-31 21:25:02 -0500

    Updated build date

--- Gruntfile.js
+++ Gruntfile.js
@@ -217,7 +217,7 @@ module.exports = function (grunt) {
                     uglify2: {
                         output: {
                             comments: false,
-                            preamble: '/* <%= package.name %> - v<%= package.version %> - ' + '2016-02-07 */'
+                            preamble: '/* <%= package.name %> - v<%= package.version %> - ' + '2016-11-01 */'
                         },
                         compress: {
                             drop_console: true    //discard calls to console.* functions
