028eb274784b1770cd4002670048586dea21247e
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-12-07 21:18:38 -0600

    Augmented comments in eslint config example and added word to about description

--- app/content.js
+++ app/content.js
@@ -58,7 +58,10 @@ define(function(require, exports) {
             title: 'Code Quality Rules',
             subtitle: '"Prime Grade" ESLint Shareable Config',
             example: code(
-                '//.eslintrc.js file in project root',
+                '// .eslintrc.js',
+                '// -> Curated selection of rules',
+                '// -> Maximum auto-fix capability',
+                '// -> Secure coding practices baked in',
                 'module.exports = {',
                 '\textends: \'omaha-prime-grade\'',
                 '};'
@@ -76,7 +79,7 @@ define(function(require, exports) {
                 quote('Practice patterns before plug-ins.'),
                 paragraph('Software developers encode the patterns of reality.  We should understand patterns to accomplish tasks and have the ability to choose the appropiate tool for any task.  As tools change, it is the skill and privilege of software developers to learn and adapt, as required.'),
                 quote('The process of writing quality code must be sustainable in order to be viable in real-world situations.'),
-                paragraph('"A developer went out to craft some code.  As he wrote his code, some of it was un-used and became dead code.  Other code was used right away.  But the code did not have tests and soon became impossible to change as requirements evolved.  Other code was added to old production code that was bloated, and not well-maintained.  The legacy code smothered the tender new code and it became un-used.  Still other code was added to a well-maintained code base with numerous well-written tests and quality metrics.  The code grew and was re-factored to be thirty, sixty, and even a hundred times more useful than when it was written."'),
+                paragraph('"A developer went out to craft some code.  As he wrote his code, some of it was un-used and became dead code.  Other code was used right away.  But the code did not have tests and soon became impossible to change as requirements evolved.  Other code was added to old production code that was bloated, and not well-maintained.  The legacy code smothered the tender new code and it became un-used.  Still other code was added to a well-maintained code base with numerous well-written tests and quality metrics.  The code grew and was re-factored to be thirty, sixty, and even a hundred times more useful than when it was first written."'),
                 list('Credits',
                     [
                         '[Nebraska Font by Daniel Heikkinen](http://www.dafont.com/nebraska.font)',
