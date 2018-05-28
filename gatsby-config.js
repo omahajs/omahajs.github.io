7c526790161df451d8ddae0e819d2ab0972d630f
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2018-05-28 16:01:11 -0500

    Add drift

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,22 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Omaha JS – Good Life. Great Code.',
-  },
-  plugins: [
-      'gatsby-plugin-react-helmet',
-      'gatsby-plugin-emotion',
-      {
-          resolve: 'gatsby-source-filesystem',
-          options: {
-              name: 'img',
-              path: `${__dirname}/src/images`
-          }
-      },
-      'gatsby-transformer-sharp',
-      'gatsby-plugin-sharp'
-  ],
+    siteMetadata: {
+        title: 'Omaha JS – Good Life. Great Code.'
+    },
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-emotion', {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'img',
+                path: `${__dirname}/src/images`
+            }
+        }, {
+            resolve: 'gatsby-plugin-drift',
+            options: {
+                appId: 'wesbmvp2s8n6'
+            }
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp'
+    ]
 }
