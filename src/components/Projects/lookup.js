3dad1610e064136efc501b2327a34ead9164bc19
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2018-05-28 15:17:57 -0500

    Initial site text

--- src/components/Projects/lookup.js
+++ src/components/Projects/lookup.js
@@ -3,7 +3,7 @@ exports.data = {
         order: 0,
         url: 'https://github.com/omahajs/generator-omaha#quick-start',
         title: 'Craft a web app from scratch',
-        description: 'description',
+        description: 'Scaffold a composite architecture that leverages string interpolation templates and a central event aggregator (includes a build chain for linting, testing and bundling).',
         technology: [
             ['marionette', 'https://marionettejs.com/'],
             ['lodash', 'https://lodash.com/docs/4.17.10'],
@@ -14,7 +14,7 @@ exports.data = {
         order: 0,
         url: 'https://github.com/omahajs/generator-omaha/blob/master/GENERATORS.md#native',
         title: 'Create a desktop app from scratch',
-        description: 'description',
+        description: 'Employ the same Omaha web app architecture to craft a native desktop app.',
         technology: [
             ['electron', 'https://electronjs.org/']
         ]
@@ -23,7 +23,7 @@ exports.data = {
         order: 0,
         url: 'https://github.com/omahajs/generator-omaha/blob/master/GENERATORS.md#server',
         title: 'Scaffold a secure Node.js server',
-        description: 'description',
+        description: 'Scaffold a light-weight Node.js with security standards "baked in"',
         technology: [
             ['express', 'https://expressjs.com/'],
             ['nsp', 'https://github.com/nodesecurity/nsp'],
@@ -39,7 +39,7 @@ exports.data = {
         order: 0,
         url: 'https://github.com/omahajs/eslint-config-omaha-prime-grade#getting-started',
         title: 'Add standards to an existing project',
-        description: 'description',
+        description: 'Use the omaha-prime-grade ESLint config in any JS project',
         technology: [
             ['eslint', 'https://eslint.org/'],
             ['eslint-plugin-compat', 'https://github.com/amilajack/eslint-plugin-compat'],
