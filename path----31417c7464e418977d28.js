d10b73c45269088b14f8567a93e16b639b8481f6
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2018-05-28 21:02:56 -0500

    Clean out public folder and move CNAME file into static directory

--- path----31417c7464e418977d28.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{87:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Omaha JS – Good Life. Great Code."}}},layoutContext:{}}}});
-//# sourceMappingURL=path----31417c7464e418977d28.js.map
